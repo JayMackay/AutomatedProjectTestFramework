11eb2112c9243a20c03fb6f579bdc891a220fcaf
JayMackay <jared.mackay1994@live.com>
2020-03-31 10:46:30 +0100

    Added Application Configuration & Reader

---
+++ AppConfigReader.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+
+namespace AutomationProjectTestFramework
+{
+    //Global reader for the App.config attirbutes
+    public static class AppConfigReader
+    {
+        public static readonly string BaseUrl = ConfigurationManager.AppSettings["base_url"];
+        public static readonly string ShoppingCartUrl = ConfigurationManager.AppSettings["shoppingcart_url"];
+    }
+}
