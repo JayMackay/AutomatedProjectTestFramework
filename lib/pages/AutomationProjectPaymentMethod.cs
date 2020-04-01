0a74883d5968559379a0f93166187c47df501603
JayMackay <jared.mackay1994@live.com>
2020-04-01 11:51:20 +0100

    Updated Minor Page Object Model Fixes

--- lib/pages/AutomationProjectPaymentMethod.cs
+++ lib/pages/AutomationProjectPaymentMethod.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace AutomationProjectTestFramework.lib.pages
 {
-    class AutomationPracticePaymentMethod
+    class AutomationProjectPaymentMethod
     {
         private IWebDriver _driver;
         public AutomationProjectPaymentMethod(IWebDriver driver)
