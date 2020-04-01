0e819dbbfdaa31eb888107f6ec250e6903c2489a
JayMackay <jared.mackay1994@live.com>
2020-04-01 10:49:04 +0100

    Added Shipping Page Object Model

---
+++ lib/pages/AutomationProjectShippingPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationProjectTestFramework.lib.pages
+{
+    class AutomationProjectShippingPage
+    {
+        private IWebDriver _driver;
+        private string _homePageUrl = AppConfigReader.BaseUrl;
+
+        //PROCEED TO CHECK OUT NAVIGATION
+        private IWebElement ProceedTocheckoutButton => this._driver.FindElement(By.LinkText("http://automationpractice.com/index.php?controller=order&amp;step=1"));
+
+        //CONTINUE SHOPPING NAVIGATION
+        private IWebElement ContinueShoppingButton => this._driver.FindElement(By.LinkText("http://automationpractice.com/index.php"));
+
+        //AGREE TERMS OF SERVICE
+        private IWebElement ContinueShoppingButton => this._driver.FindElement(By.Class("Clicked"));
+
+        public AutomationProjectShoppingCartPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void VisitHomePage()
+        {
+            _driver.Navigate().GoToUrl(_homePageUrl);
+        }
+
+        public void ClickProceedToCheckOutLink()
+        {
+            ProceedTocheckoutButton.Click();
+        }
+    }
+}
