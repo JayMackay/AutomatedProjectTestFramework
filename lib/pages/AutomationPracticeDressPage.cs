88e33d3cc81d7e6e091c98831b9f4c3024e0f262
hamzah khan <khanha3@aston.ac.uk>
2020-04-01 10:48:11 +0100

    deleted confilting files

--- lib/pages/AutomationPracticeDressPage.cs
+++ lib/pages/AutomationPracticeDressPage.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQa.Selenium;
+using OpenQA.Selenium;
 
 namespace AutomationProjectTestFramework.lib.pages
 {
