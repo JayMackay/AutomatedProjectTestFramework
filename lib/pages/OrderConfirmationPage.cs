fa653e89ea913c47834344e482207f2d85f38a43
mitchclarke123 <mitchclarke123@hotmail.com>
2020-04-02 23:04:20 +0100

    Added Order Confirmation page POM and finished MVP Test

---
+++ lib/pages/OrderConfirmationPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using NUnit;
+
+namespace AutomationProjectTestFramework.lib.pages
+{
+    public class AutomationProjectConfirmationPage
+    {
+        private IWebDriver _driver;
+        private string _homePageUrl = AppConfigReader.BaseUrl;
+        
+        //CONFIRM ORDER BUTTON
+        private IWebElement OrderConfirmationMessage => this._driver.FindElement(By.XPath("/html/body/div[1]/div[2]/div/div[3]/div/div/p/strong"));
+
+        public AutomationProjectConfirmationPage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public void VisitHomePage()
+        {
+            _driver.Navigate().GoToUrl(_homePageUrl);
+        }
+
+        public string ConfirmOrderConfirmationMessage()
+        {
+            return OrderConfirmationMessage.Text;
+        }
+    }
+}
